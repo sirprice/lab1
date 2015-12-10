a552c3d26f7d022d7c9e74438b154e895e3476b9
sirprice <carljohan.dahlman@gmail.com>
2015-12-10 12:29:24 +0100

    sdaf

--- src/enums/AlbumGenre.java
+++ src/enums/AlbumGenre.java
@@ -4,5 +4,5 @@ package enums;
  * Created by cj on 09/12/15.
  */
 public enum AlbumGenre {
-    ROCK,POP,RandB,DANCE,HIPHOP,CLASSIC,METAL,JAZZ
+    ROCK,POP,RandB,DANCE,HIPHOP,CLASSIC,METAL,JAZZ,BLUES
 }
